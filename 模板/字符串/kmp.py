fd5518cd88af8628a9263e931aee0ed9c80af6e2
apl43 <56750515+apl43@users.noreply.github.com>
2024-04-20 22:50:28 +0800

    Add files via upload

---
+++ 模板/字符串/kmp.py
@@ -0,0 +1,36 @@
+def kmp(text, pattern):
+    # 求解 pi 数组
+    m = len(pattern)
+    pi = [0] * m
+    st = 0
+    for i in range(1, m):
+        v = pattern[i]
+        while st and pattern[st] != v:
+            st = pi[st - 1]
+        if pattern[st] == v:
+            st += 1
+        pi[i] = st
+    
+    st = 0
+    # 输出多个可重叠的 pattern 下标
+    # res = []
+
+    for i, v in enumerate(text):
+        while st and pattern[st] != v:
+            st = pi[st - 1]
+        if pattern[st] == v:
+            st += 1
+
+    # 输出一个
+        if st == len(pattern):
+            return i - m + 1
+    return -1
+            
+    # 输出多个
+    #     if st == len(pattern):
+    #         res.append(i - m + 1)
+    #         st = pi[st - 1]
+    # return res
+
+
+print(kmp("abacabab", "abab"))
---
+++ 模板/数据结构/UnionFind.py
@@ -0,0 +1,22 @@
+class UnionFind():
+    def __init__(self, size):
+        self.fa = list(range(size))
+    
+    def find(self, x):
+        # # 递归方式
+        # if self.fa[x] != x:
+        #     self.fa[x] = self.find(self.fa[x])
+        # return self.fa[x]
+        
+        # 非递归方式
+        y = x
+        while self.fa[y] != y:
+            y = self.fa[y]
+        
+        while self.fa[x] != y:
+            x = self.fa[x]
+            self.fa[x] = y
+        return self.fa[y]
+        
+    def union(self, x, y): # x 连接到 y 上
+        self.fa[self.find(x)] = self.find(y)
