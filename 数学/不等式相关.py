60c806912cae91f2283fbff4028d00b62b635a3e
apl43 <56750515+apl43@users.noreply.github.com>
2024-09-23 12:54:56 +0800

    Update 不等式相关.py

--- 数学/不等式相关.py
+++ 数学/不等式相关.py
@@ -7,4 +7,5 @@ x<<i > s  =>  x > s>>i    x<<i ≥ s  =>  x > (s-1)>>i
 
 1<<x ≤ v  =>  x ≤ v.bit_length()-1    1<<x < v  =>  x ≤ (v-1).bit_length()-1
 1<<x > v  =>  x ≥ v.bit_length()      1<<x ≥ v  =>  x ≥ (v-1).bit_length()
+
 """
