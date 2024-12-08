806dd0e3bc5a5041ee222d7a5104773a61994b8f
apl43 <1397381851@qq.com>
2024-12-08 16:46:13 +0800

    更新

--- 字符串/kmp.py
+++ 字符串/kmp.py
@@ -26,3 +26,24 @@ def kmp(text, pattern):
 
 print(kmp("abacabab", "abab"))
 print(kmp([1, 2, 1, 3, 1, 2, 1, 2], [1, 2, 1, 2]))
+
+
+
+# 另一种方法，只用求一次 pi 数组
+def kmp(text, pattern):
+    s = pattern + "#" + text
+    m = len(s)
+    pi = [0] * m
+    st = 0
+    for i in range(1, m):
+        v = s[i]
+        while st and s[st] != v:
+            st = pi[st - 1]
+        if s[st] == v:
+            st += 1
+        pi[i] = st
+    return [i - len(pattern) * 2 for i, x in enumerate(pi) if x == len(pattern)]
+
+
+print(kmp("abacabab", "abab"))
+# print(kmp([1, 2, 1, 3, 1, 2, 1, 2], [1, 2, 1, 2]))
