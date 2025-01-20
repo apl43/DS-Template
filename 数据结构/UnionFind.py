db990287710d945965ccf6417242cfad9f43ea46
apl43 <1397381851@qq.com>
2025-01-20 12:23:35 +0800

    update

--- 数据结构/UnionFind.py
+++ 数据结构/UnionFind.py
@@ -20,3 +20,28 @@ class UnionFind:
         
     def union(self, x, y): # x 连接到 y 上
         self.fa[self.find(x)] = self.find(y)
+
+
+class UnionFind:
+    def __init__(self, size):
+        self.fa = list(range(size))
+        self.size = [1] * size
+    
+    def find(self, x):
+        y = x
+        while self.fa[y] != y:
+            y = self.fa[y]
+        
+        while self.fa[x] != y:
+            x = self.fa[x]
+            self.fa[x] = y
+        return self.fa[y]
+        
+    def union(self, x, y):
+        x, y = self.find(x), self.find(y)
+        if x == y:
+            return
+        if self.size[x] < self.size[y]:
+            x, y = y, x
+        self.fa[y] = x
+        self.size[x] += self.size[y]
